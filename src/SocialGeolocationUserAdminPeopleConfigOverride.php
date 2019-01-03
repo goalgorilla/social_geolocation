c346b82215612006f8bb87cd3afbe616b42fdcf0
ronaldtebrake <ronald@goalgorilla.com>
2019-01-03 11:05:35 +0100

    Make sure admin/people filter works, improved readability of YAML override and dependencies

--- src/SocialGeolocationUserAdminPeopleConfigOverride.php
+++ src/SocialGeolocationUserAdminPeopleConfigOverride.php
@@ -6,6 +6,7 @@ use Drupal\Core\Cache\CacheableMetadata;
 use Drupal\Core\Config\ConfigFactoryInterface;
 use Drupal\Core\Config\ConfigFactoryOverrideInterface;
 use Drupal\Core\Config\StorageInterface;
+use Drupal\Core\Serialization\Yaml;
 
 /**
  * Example configuration override.
@@ -46,98 +47,114 @@ class SocialGeolocationUserAdminPeopleConfigOverride implements ConfigFactoryOve
     $config_name = 'views.view.user_admin_people';
 
     if (in_array($config_name, $names)) {
-
       // Grab current configuration and push the new values.
       $config = $this->configFactory->getEditable($config_name);
       // We have to add config dependencies to field storage.
       $dependencies = $config->getOriginal('dependencies', FALSE)['module'];
       $dependencies[] = 'social_geolocation';
-
+      $dependencies[] = 'geolocation';
       $overrides[$config_name]['dependencies']['module'] = $dependencies;
 
-      /** @TODO Get dependencies array + add to it */
-      $overrides[$config_name]['display']['default'] = [
-        'display_options' => [
-          'group_by' => TRUE,
-          'filters' => [
-            'field_profile_geolocation_proximity' => [
-              'value' => [
-                'min' => '',
-                'max' => '',
-                'value' => '',
-              ],
-              'admin_label' => '',
-              'operator' => '<=',
-              'proximity_source' => 'exposed',
-              'proximity_lat' => '',
-              'proximity_lng' => '',
-              'proximity_units' => 'km',
-              'proximity_argument' => '',
-              'entity_id_argument' => '',
-              'boundary_filter' => '',
-              'client_location' => false,
-              'id' => 'field_profile_geolocation_proximity',
-              'table' => 'profile__field_profile_geolocation',
-              'field' => 'field_profile_geolocation_proximity',
-              'relationship' => 'profile',
-              'group_type' => 'group',
-              'group' => '1',
-              'exposed' => true,
-              'expose' => [
-                'operator_id' => 'field_profile_geolocation_proximity_op',
-                'label' => 'Distance in kilometers',
-                'description' => '',
-                'use_operator' => false,
-                'operator' => 'field_profile_geolocation_proximity_op',
-                'identifier' => 'field_profile_geolocation_proximity',
-                'required' => false,
-                'remember' => false,
-                'multiple' => false,
-                'remember_roles' => [
-                  'authenticated' => 'authenticated',
-                  'anonymous' => '0',
-                  'administrator' => '0',
-                  'contentmanager' => '0',
-                  'sitemanager' => '0',
-                ],
-                'placeholder' => '',
-                'min_placeholder' => '',
-                'max_placeholder' => '',
-                'input_by_geocoding_widget' => 0,
-                'geocoder_plugin_settings' => [
-                  'plugin_id' => 'google_geocoding_api',
-                  'settings' => [
-                    'components' => [
-                      'route' => '',
-                      'locality' => '',
-                      'administrativeArea' => '',
-                      'postalCode' => '',
-                      'country' => '',
-                    ],
-                  ],
-                ],
-              ],
-              'is_grouped' => false,
-              'group_info' => [
-                'label' => '',
-                'description' => '',
-                'identifier' => '',
-                'optional' => true,
-                'widget' => 'select',
-                'multiple' => false,
-                'remember' => false,
-                'default_group' => 'All',
-                'default_group_multiple' => [],
-                'group_items' => [],
-              ],
-              'plugin_id' => 'geolocation_filter_proximity',
-            ],
-          ],
-        ],
-      ];
+      $filters = $config->getOriginal('display', FALSE)['default']['display_options']['filters'];
+
+      $filters['field_profile_geolocation_proximity'] = $this->getAdminConfigFilter();
+      $overrides[$config_name]['display']['default']['display_options']['filters'] = $filters;
     }
 
     return $overrides;
+  }
+
+  public function getAdminConfigFilter() {
+    $overrides = <<<YAML
+id: field_profile_geolocation_proximity
+table: profile__field_profile_geolocation
+field: field_profile_geolocation_proximity
+relationship: profile
+group_type: group
+admin_label: ''
+operator: '<='
+value:
+  min: ''
+  max: ''
+  value: '0'
+  center: ''
+group: 1
+exposed: true
+expose:
+  operator_id: field_profile_geolocation_proximity_op
+  label: 'Proximity (field_profile_geolocation)'
+  description: ''
+  use_operator: false
+  operator: field_profile_geolocation_proximity_op
+  identifier: field_profile_geolocation_proximity
+  required: false
+  remember: false
+  multiple: false
+  remember_roles:
+    authenticated: authenticated
+    anonymous: '0'
+    administrator: '0'
+    contentmanager: '0'
+    sitemanager: '0'
+  placeholder: ''
+  min_placeholder: ''
+  max_placeholder: ''
+is_grouped: false
+group_info:
+  label: ''
+  description: ''
+  identifier: ''
+  optional: true
+  widget: select
+  multiple: false
+  remember: false
+  default_group: All
+  default_group_multiple: {  }
+  group_items: {  }
+location_input:
+  client_location:
+    weight: 0
+    enable: false
+    location_input_id: client_location
+    settings:
+      auto_submit: false
+      hide_form: false
+  coordinates:
+    enable: true
+    weight: 0
+    location_input_id: coordinates
+  geocoder:
+    settings:
+      plugin_id: nominatim
+      settings:
+        label: Address
+        description: 'Enter an address to be localized.'
+      auto_submit: false
+      hide_form: false
+    weight: 0
+    enable: false
+    location_input_id: geocoder
+  'fixed_value:fixed_value':
+    settings:
+      location_settings:
+        settings:
+          latitude: ''
+          longitude: ''
+      location_plugin_id: fixed_value
+    weight: 0
+    enable: false
+    location_input_id: location_plugins
+  'freeogeoip:freeogeoip':
+    weight: 0
+    enable: false
+    location_input_id: location_plugins
+    settings:
+      location_plugin_id: freeogeoip
+unit: km
+plugin_id: geolocation_filter_proximity
+YAML;
+
+    return Yaml::decode($overrides);
   }
 
   /**
